33d90e83cd7dfd24f4520de569fdf09d5c249855
hota1024 <hotalog@hota1024.com>
2020-04-30 21:43:18 +0900

    Add server/middleware/cors.ts

---
+++ server/middleware/cors.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const cors = (req: Request, res: Response, next: NextFunction) => {
+  // Allow Origins
+  res.header('Access-Control-Allow-Origin', '*')
+  // Allow Methods
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  )
+  // Allow Headers
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, Accept, Content-Type, Authorization'
+  )
+  // Handle preflight, it must return 200
+  if (req.method === 'OPTIONS') {
+    // Stop the middleware chain
+    return res.status(200).end()
+  }
+  // Next middleware
+  next()
+}
