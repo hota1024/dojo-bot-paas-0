39e81a7437dcf15952f5ed2dbdbe35b9139a51c4
hota1024 <hotalog@hota1024.com>
2020-04-30 17:26:12 +0900

    Update server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,7 @@
-import next from 'next'
-import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+import { startNextApp } from './next'
+import { startProbot } from './probot'
+dotenv.config()
 
-const port = parseInt(process.env.PORT ?? '3000')
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
-
-app.prepare().then(() => {
-  const server = express()
-  server.get('*', (req: Request, res: Response) => {
-    return handle(req, res)
-  })
-
-  server.listen(port, (error?: Error) => {
-    if (error) throw error
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+startProbot()
+startNextApp()
